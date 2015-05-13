a35efa9d80775f457914ac70ab49c0589cdf1068
Jason Mimick <jmimick@gmail.com>
2015-05-12 22:14:28 -0500

    pem for training

--- MatchCStudents.js
+++ MatchCStudents.js
@@ -10,26 +10,26 @@ var p1 = db.runCommand(
                 { "score" : { $lte : 79 } } 
             ]
         } 
-    }, { 
+    } , { 
         $group : {
             _id : "$score",
             student : { $addToSet : "$student" }
         }
-    }, {
+    } , {
         $unwind : "$student"
-    }, { 
+    } , { 
         $group : {
             _id : "$student",
             C_score_count : { $sum : 1 }
         }
-    }, {
+    } , {
         $match : { C_score_count : { $gte : 2 } }
-    }, {
+    } , {
         $group : {
             _id : "$C_score_count",
             students : { $addToSet : "$_id" }
         }
-    }
+    } 
 ]});
 printjson(p1);
 
