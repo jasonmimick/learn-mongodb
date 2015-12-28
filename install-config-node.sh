0b1186f362096b67f0d574ddaa65e13bb2d91588
Jason Mimick <jmimick@gmail.com>
2015-12-28 13:53:17 -0500

    Update install-config-node.sh

--- install-config-node.sh
+++ install-config-node.sh
@@ -6,4 +6,4 @@ sudo yum -y install mongo-10gen-server-2.4.10-mongodb_1.x86_64.rpm
 sudo mkdir -p /data/configdb
 sudo chown ec2-user:ec2-user /data/configdb
 sudo chown ec2-user:ec2-user /data
-mongod --logpath /data/db/monogd.log --fork --port 27000 --configsvr
+mongod --logpath /data/configdb/monogd.log --fork --configsvr
