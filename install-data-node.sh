fa88610f1a1fd999d9d86d11be1d23f31ca89e97
Jason Mimick <jmimick@gmail.com>
2015-12-28 12:46:10 -0500

    Update install-data-node.sh

--- install-data-node.sh
+++ install-data-node.sh
@@ -8,7 +8,7 @@ sudo yum -y install mongo-10gen-server-2.4.10-mongodb_1.x86_64.rpm
 sudo mkdir -p /data/db
 sudo chown ec2-user:ec2-user /data/db
 sudo chown ec2-user:ec2-user /data
-mongod --logpath /data/db/monogd.log --fork --port 27000
+mongod --logpath /data/db/monogd.log --fork --port 27000 --replSet $REPL_SET
 
 
 
