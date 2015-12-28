29c505df2b0ab68fa692d2b5b9618a717b107643
Jason Mimick <jmimick@gmail.com>
2015-12-28 13:51:46 -0500

    Create install-config-node.sh

---
+++ install-config-node.sh
@@ -0,0 +1,9 @@
+curl -O http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/RPMS/mongo-10gen-2.4.10-mongodb_1.x86_64.rpm
+curl -O http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/RPMS/mongo-10gen-server-2.4.10-mongodb_1.x86_64.rpm
+sudo yum -y install mongo-10gen-2.4.10-mongodb_1.x86_64.rpm
+sudo yum -y install mongo-10gen-server-2.4.10-mongodb_1.x86_64.rpm
+
+sudo mkdir -p /data/configdb
+sudo chown ec2-user:ec2-user /data/configdb
+sudo chown ec2-user:ec2-user /data
+mongod --logpath /data/db/monogd.log --fork --port 27000 --configsvr
