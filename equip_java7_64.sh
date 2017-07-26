9b5ee2ae1413eb33507e6b981d35248de3fc77d5
Jason Mimick <jmimick@gmail.com>
2017-07-26 11:45:06 -0400

    Create equip_java7_64.sh

---
+++ equip_java7_64.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#
+# Ubuntu Equip 
+#  Java 7 64-bit Equip
+# Licence: MIT
+# to run: wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java7_64.sh && bash equip_java7_64.sh
+
+if [ -d "/usr/lib/jvm/" ]; then
+	echo "There's already an installation of Java JDK in /usr/lib/jvm"
+	rm -f equip_java7_64.sh
+	echo "Skipping..."
+	exit 0
+fi
+
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
+
+sudo apt-get install curl -y 
+
+curl -L --cookie "oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u65-b17/jdk-7u65-linux-x64.tar.gz -o jdk-7-linux-x64.tar.gz
+tar -xvf jdk-7-linux-x64.tar.gz
+
+sudo mkdir -p /usr/lib/jvm
+sudo mv ./jdk1.7.* /usr/lib/jvm/
+
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0_65/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0_65/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7.0_65/bin/javaws" 1
+
+sudo chmod a+x /usr/bin/java 
+sudo chmod a+x /usr/bin/javac 
+sudo chmod a+x /usr/bin/javaws
+sudo chown -R root:root /usr/lib/jvm/jdk1.7.0_65
+
+rm jdk-7-linux-x64.tar.gz
+rm -f equip_base.sh 
+rm -f equip_java7_64.sh
+
+java -version
